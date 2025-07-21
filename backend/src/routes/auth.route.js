d5c0689e8855a78ffec126b06b06b11ddbf965dc
Pankaj Sharma <grgnld3970@gmail.com>
2025-07-21 18:07:58 +0530

    added authentication

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { register_user, login_user } from '../controllers/auth.controllers.js';
+import { authMiddleware } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/register', register_user);
-router.post('/login', login_user);
+router.post('/login', authMiddleware, login_user);
 export default router;      
